c1af705b35340b349d446bf5faa3e27261ce8254
Norma Leslie Martinez Silva <normalesliemartinezsilva@MacBook-Pro-de-Norma.local>
2023-07-07 10:01:41 -0600

    Se agregan descripciones

--- TAREA1.py
+++ TAREA1.py
@@ -1,3 +1,4 @@
+#Esta seccion es una bienvenida
 print("Bienvenido a NORMASCORP")
 while True:
     respuesta=str(input("¿Deseas conocer tu masa corporal? S (SI) / N (NO): "))
@@ -15,6 +16,7 @@ while True:
         exit()
     else: 
         break
+ #Aqui solicitamos el nombre, pero tiene validaciones, la primera es de espacio, la segunda por numeros y la tercera es caracteres especiales    
 while True:
     nombre = str(input("Ingresa tu nombre, por favor: "))
     if nombre == "":
@@ -25,6 +27,7 @@ while True:
         print("Tipo de dato no reconocido")
     else: 
         break
+#Aqui de igual forma se pide el apellido y se realizan las mismas validaciones
 while True:
     apellidop=str(input("Ingresa tus apellido paterno, por favor: "))
     if apellidop == "":
@@ -35,6 +38,7 @@ while True:
         print("Tipo de dato no reconocido")
     else :
             break
+#Aqui de igual forma se solicita el apellido materno y tambien cuenta con las validaciones
 while True:
     apellidom=str(input("Ingresa tus apellido materno, por favor: "))
     if apellidom == "":
@@ -44,9 +48,14 @@ while True:
     elif apellidom.isalpha() == False: 
         print("Tipo de dato no reconocido")
     else :
-            break  
-print("Mucho gusto en conocerte ", nombre + " " + apellidop + " " + apellidom )
-print("Para poder dar tus resultados de masa corporal necesito que me proporciones mas datos para continuar")  
+            break 
+#Aqui se imprime una bienvenida con los datos que previamente el usuario ingreso , adicional se realiza el metodo capitalize para que se muestren las mayusculas decada palabra
+print("Mucho gusto en conocerte ", nombre.capitalize() + " " + apellidop.capitalize() + " " + apellidom.capitalize() )
+print("Para poder dar tus resultados de masa corporal necesito que me proporciones mas datos para continuar")
+#Aqui iniciamos con la funcionalidad importante de programa, que es solicitar cada valor para realizar el calculo de masa
+# las validaciones estan basadas con el tipo de dato que se ingresa para la edad se espera siempre un entero
+# que no sea uno negativo.
+# Para los de peso y estatura se busca un valos donde se apecten decimales, pero si es diferente no te deja avanzar  
 while True:
     try:
         edad=int(input("me puedes dar tu edad, por favor:  "))
@@ -59,7 +68,7 @@ while True:
         break
 while True:
     try:
-        peso=float(input("me puedes dar tu peso, por favor:  " ))
+        peso=float(input("me puedes dar tu peso, en kilos, por favor:  " ))
     except ValueError:   
         print("Tipo de dato no reconocido, solo se permite número entero mayor a 1") 
         continue
@@ -69,7 +78,7 @@ while True:
         break 
 while True:
     try:
-        estatura=float(input("me puedes dar tu estatura, por favor:  " ))
+        estatura=float(input("me puedes dar tu estatura, en metros, por favor:  " ))
     except ValueError:   
         print("Tipo de dato no reconocido, solo se permite número entero mayor a 1") 
         continue
@@ -77,24 +86,25 @@ while True:
          print("Tipo de dato no reconocido, solo se permite número entero mayor a 1") 
     else:
         break    
+#Aqui se realiza el calculo y las validaciones de los resultados, dependiendo al valor que se ingreso previamente
 masacorporal= peso/estatura**2 
-print("Hola ",nombre," tu edad es: ",edad, " tu peso es: ", peso, "tu estatura es: ", estatura, " tu masa corporal es: ", str(masacorporal))
+print("\n \t Hola ",nombre.capitalize(),"\n \t tu edad es: ",edad, "\n \t tu peso es: ", peso, "\n \t tu estatura es: ", estatura, "\n \t tu masa corporal es: ", str(masacorporal))
 if masacorporal >= 0 and masacorporal <= 15.99 :
-    print ("Delgadez severa")
+    print ("\t Delgadez severa")
 elif masacorporal >= 16.00 and masacorporal <= 16.99 : 
-    print ("Delgadez moderada")
+    print ("\t Delgadez moderada")
 elif masacorporal >= 17.00 and masacorporal <= 18.49 :
-    print ("Delgadez leve")
+    print ("\t Delgadez leve")
 elif masacorporal >= 18.50 and masacorporal <= 24.99 :
-    print ("Normal")
+    print ("\t Normal")
 elif masacorporal >= 25.00 and masacorporal <= 29.99 :
-    print ("Sobrepeso")
+    print ("\t Sobrepeso")
 elif masacorporal >= 30.00 and masacorporal <= 34.99 :
-    print ("obesidad leve")
+    print ("\t obesidad leve")
 elif masacorporal >= 35.00 and masacorporal <= 39.00 :
-    print ("obesidad media")
+    print ("\t obesidad media")
 elif masacorporal >= 40.00 :
-    print ("obesidad morbida")
+    print ("\t obesidad morbida")
 else: 
     masacorporal >= -1 
     print ("Fuera de rango, no se aceptan numeros negativos")    
